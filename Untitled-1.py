88fa821cc7fea5cd2e72a581e8528a38d34d1db7
alexkilo07 <alex.quilodran@duocuc.cl>
2025-04-22 09:29:10 -0400

    segundo commit

---
+++ Untitled-1.py
@@ -0,0 +1,12 @@
+print("Ingrese su nombre")
+nombre=input()
+print("Ingrese su edad")
+
+edad=int(input)
+
+print("Hola", nombre)
+
+if edad<18:
+    print("Usted es menor de edad")
+else:
+     print("Usted es mayora de edad")
